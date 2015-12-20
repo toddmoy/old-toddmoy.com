46439c3b676e2730accae80913695a7f8b15d8db
toddmoy <todd.moy@gmail.com>
2015-12-20 12:31:03 -0700

    Add smoothState

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,8 @@
 //= require "jquery-2.1.4.min"
+//= require "jquery.smoothState"
 //= require "jquery.unveil"
 
 $(document).ready(function(){
   $("img").unveil(200);
+  $("#main").smoothState();
 });

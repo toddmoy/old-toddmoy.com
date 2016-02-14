c0874caa5bb234c3bcb50d4111594fc488d51648
toddmoy <todd.moy@gmail.com>
2016-02-14 10:04:33 -0700

    Many more changes.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,20 @@
 //= require "jquery/dist/jquery.min"
-//= require "smoothstate/jquery.smoothState.min"
 //= require "unveil/jquery.unveil.min"
-//= require "twentytwenty/js/jquery.twentytwenty"
-//= require "twentytwenty/js/jquery.event.move"
+//= require "jquery-visible/jquery.visible.min"
+// require "twentytwenty/js/jquery.twentytwenty"
+// require "twentytwenty/js/jquery.event.move"
+// require "smoothstate/jquery.smoothState.min"
 
 $(document).ready(function(){
   $('img').unveil(200);
-  $('#main').smoothState();
+  // $('#main').smoothState();
+});
+
+$(window).scroll(function(event) {
+  $(".case-story__chapter").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
+    }
+  });
 });

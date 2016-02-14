0eb12d33dcbca13de2c24b4688b9c293b0d3e84f
toddmoy <todd.moy@gmail.com>
2016-02-14 16:55:57 -0700

    Remove fancy translation effect and jquery-visible package. Resolves #17

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,5 @@
 //= require "jquery/dist/jquery.min"
 //= require "unveil/jquery.unveil.min"
-//= require "jquery-visible/jquery.visible.min"
 // require "twentytwenty/js/jquery.twentytwenty"
 // require "twentytwenty/js/jquery.event.move"
 // require "smoothstate/jquery.smoothState.min"
@@ -8,13 +7,4 @@
 $(document).ready(function(){
   $('img').unveil(200);
   // $('#main').smoothState();
-});
-
-$(window).scroll(function(event) {
-  $(".case-story__chapter").each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("come-in");
-    }
-  });
 });

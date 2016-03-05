676676ffd1695f478c30164dccb685ef23ef545c
toddmoy <todd.moy@gmail.com>
2016-03-05 09:22:59 -0700

    Remove TwentyTwenty and SmoothState.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,6 @@
 //= require "jquery/dist/jquery.min"
 //= require "unveil/jquery.unveil.min"
-// require "twentytwenty/js/jquery.twentytwenty"
-// require "twentytwenty/js/jquery.event.move"
-// require "smoothstate/jquery.smoothState.min"
 
 $(document).ready(function(){
   $('img').unveil(200);
-  // $('#main').smoothState();
 });
